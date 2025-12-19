e6db7a0537b8e576acb402da041fbae2520cf5fc
Pyae Zin Phyo <u6715150@au.edu>
2025-12-19 11:15:12 +0700

    react-assignment Done

---
+++ react-assignment/src/App.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react' 
+import reactLogo from './assets/react.svg' 
+import viteLogo from '/vite.svg' 
+import './App.css' 
+import User from './User.jsx'
+function App() { 
+ 
+  return ( 
+  <> 
+    <div className='card'> 
+      <User message="PYAE ZIN PHYO"/>
+    </div> 
+  </> 
+  ) 
+} 
+export default App 
