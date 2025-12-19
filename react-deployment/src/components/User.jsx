7108f4f0bde3bb13c2795b9a20e6b28bca372491
Pyae Zin Phyo <u6715150@au.edu>
2025-12-19 11:42:08 +0700

    Update react component

---
+++ react-deployment/src/components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const User = ({ message }) => {
+  return (
+    <div>
+        <h1>Hello, React !</h1>
+        <p>{message}</p>
+    </div>
+
+  )
+}
+
+export default User
